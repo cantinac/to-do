29b41e460250dabfaf7efa6829beef84289f2243
Wayne Elgin <wayne.elgin@gmail.com>
2013-04-19 14:55:20 -0400

    minified and concatenated js and css files

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,9 +1,4 @@
 'use strict';
 
 /* Directives */
-angular.module('todoApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+
