4ec5d8738466d1dc12514f3b1c5eeb9d58d9a0b2
Sam Selikoff <sam.selikoff@gmail.com>
2014-01-14 23:12:21 -0500

    Adding Ember data, updating models

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 App = Ember.Application.create();
 
 // Models
-App.Todo = Ember.Object.extend({});
+App.Todo = DS.Model.extend({});
 
 // Routes
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
-    return [App.Todo.create({title: "My first todo", isDone: false})];
+    return this.store.all('todo');
   }
+});
+
+// Controllers
+App.ApplicationController = Ember.Controller.extend({
+
+  addTodo: function() {
+    this.store.createRecord('todo', {
+      title: 'Hi there',
+      isDone: false
+    });
+  }
+
 });
