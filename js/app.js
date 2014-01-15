5d7599f56f41b1d785cfe7f31d2affbaf77c353d
Sam Selikoff <sam.selikoff@gmail.com>
2014-01-14 22:33:26 -0500

    Creating models

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
-});
+// Models
+App.Todo = Ember.Object.extend({});
 
+// Routes
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return [App.Todo.create({title: "My first todo", isDone: false})];
   }
 });
