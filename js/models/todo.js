44abd24e277e0445bfc7679be04b0dca643a12c8
Francois Laberge <francoislaberge@gmail.com>
2014-02-10 19:55:48 -0500

    Todo app based on ember.js tutorial. Customizing to match spec next

---
+++ js/models/todo.js
@@ -0,0 +1,26 @@
+(function(){
+
+Todos.Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: false
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: true
+ }
+];
+
+})();
