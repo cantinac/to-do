f57e13b483bbcf21fa4eca1a3f9a16ba270d4d73
Scott Padgett <misc@wscottpadgett.com>
2014-02-23 00:54:02 -0500

    App is largely working, with a few behavioral and visual bugs.

--- js/main.js
+++ js/main.js
@@ -6,21 +6,11 @@ if (!window.console.error) window.console.error = function() {};
 
 var Todo = (function() {
 
-    var applicationModel,
-        todoCollection;
 
     // Models
 
-    var ApplicationModel = Backbone.Model.extend({
-        initialize: function(){
-            console.log("application init'd");
-        }
-    });
-
-
     var TodoModel = Backbone.Model.extend({
         initialize: function() {
-            console.log("a todo model init'd");
         },
         defaults: {
             text: "",
@@ -31,17 +21,15 @@ var Todo = (function() {
 
     // Collections
 
-    var TodoCollection = Backbone.Collection.extend({
+    var ApplicationCollection = Backbone.Collection.extend({
             initialize: function() {
-                console.log("TodoCollection is initialized");
                 this.on("add", this.onAdd);
             },
 
             model: TodoModel,
 
-            onAdd: function() {
-                console.log("Todo added to collection");
-                applicationView.render();
+            onAdd: function(model) {
+                new TodoView(model)
             }
 
     });
@@ -53,34 +41,37 @@ var Todo = (function() {
 
         template: _.template($('#tpl-application').html()),
 
-        thisEl: {},
-
-        initialize: function() { 
-            this.listenTo(this.model, 'change', this.render);           
-            console.log("set application Views");
-            this.thisEl = $(this.el);
+        initialize: function() {       
+            this.collection.on("add", (function(that) {
+                    return function(model) {
+                        var targetUlEl = $('<li class="li-holder"></li>').appendTo(that.$el.find('#todoUl'));
+                        var thisToDo = new TodoView({ el: targetUlEl, model: model }).render();
+                        thisToDo.listenTo(thisToDo.model, "change", function() {
+                            this.render();
+                        });
+                    }
+                })(this)
+            );
             this.render();
         },
 
         render: function() {
-            console.log("should render applicationView");
-
-            this.thisEl.html(this.template());
-
-            // cycle the TodoCollection and write a todo for every one in the list
-            _.each(todoCollection.models, function(model, i) {
-                new TodoView({ el: this.thisEl.find('#todoUl').append('<li></li>') , model: model })
-            }, this); 
-
+            return this.$el.html(this.template());
         },
 
         addTodoItem: function(e) {
             e.preventDefault();
-            todoCollection.add( new TodoModel({text: this.thisEl.find('#inputField').val(), completed: "false"}) );
+            e.stopImmediatePropagation();
+            var $input = this.$el.find('#inputField');
+            if ($input.val().length > 0) {
+                this.collection.add( new TodoModel({text: $input.val(), completed: "false"}) );
+                $input.val('');
+                $input.focus();
+            }
         },
 
         events: {
-            "mouseup #addTodoButton": "addTodoItem"
+            "submit #addForm": "addTodoItem"
         }
 
     });
@@ -89,26 +80,42 @@ var Todo = (function() {
 
         template: _.template($('#tpl-todo').html()),
 
-        initialize: function() { 
-            this.render();
+        initialize: function() {
+            this.viewId = new Date().getTime();
         },
 
         render: function() {
-                console.log("should render todoView");
                 var templateArgs = { 
                     todoText: this.model.attributes.text, 
                     itemChecked: this.model.attributes.completed 
                 };
-                $(this.el).html(this.template(templateArgs));
+                this.$el.find('input[type=checkbox]').off("change", this.toggleCompleted(this));
+                this.$el.off("click", this.toggleChecked);
+                var replacement = $(this.template(templateArgs));
+                this.$el.replaceWith(replacement);
+                this.$el = replacement;
+                this.$el.find('input[type=checkbox]').on("change", this.toggleCompleted(this));
+                this.$el.on("click", this.toggleChecked);
+                return this;
         },
 
-        startHandler: function() {
-            // change viewState to Monitor
-//            applicationModel.set({activeView: "monitorView"});
+        toggleCompleted: function(that) {
+            return function(e) {
+                e.stopImmediatePropagation();
+                if (that.model.attributes.completed == "true") {
+                    that.model.set("completed", "false");
+                } else {
+                    that.model.set("completed", "true");
+                }
+            }
         },
 
-        events: {
-//            "mouseup .start-monitor": "startHandler"
+        toggleChecked: function(e) {
+            e.stopImmediatePropagation();
+            var inputEl = e.currentTarget.childNodes[1];
+            inputEl.checked ? inputEl.checked = false : inputEl.checked = true;
+            inputEl.dispatchEvent(new Event('change')); 
+            inputEl.focus();
         }
 
     });
@@ -116,143 +123,13 @@ var Todo = (function() {
 
 
 
-    // AMQ client housing
-
-    var AmqClient = function(_monitorModel, _monitorView) {
-
-
-        var _monitorModel = _monitorModel,
-            _monitorView = _monitorView,
-            fakeAMQinterval,
-            lastUpdateTime = new Date().getTime(),
-            AMQopen,
-            initialTimeOut = 2000;      
-
-        function getAthleteMetricsById(id, metricsObjectArray) {
-            for (i = metricsObjectArray.length - 1; i >= 0; i--) {
-                if (metricsObjectArray[i].athleteId == id) {
-                    return metricsObjectArray[i];
-                }
-            }
-            return false;
-        }
-
-        function writeClubModelPlayers() {
-            // dupe our dummy data templates
-            var choppedPlayerData = teamMetrics.slice();
-            var thisTeamPlayers = $.extend({},teamPlayers);
-            // randomly pick athletes to drop from data
-            for (i=0; i<6; i++) {
-                var iToDrop = Math.round(Math.random() * (choppedPlayerData.length - 1) );
-                choppedPlayerData.splice(iToDrop, 1);
-            }
-            for (j = 0; j < thisTeamPlayers.club.players.length; j++ ) {
-                var thisId = thisTeamPlayers.club.players[j].id;
-                var thisIdMetrics = getAthleteMetricsById(thisId,choppedPlayerData);
-                thisIdMetrics.disabled = false;
-                var naMetrics = {
-                    "id": thisId,
-                    "acceleration": offlineMetricOutput,
-                    "distance": offlineMetricOutput, 
-                    "power": offlineMetricOutput, 
-                    "speed": offlineMetricOutput,
-                    "disabled": "true"
-                };
-                if (thisIdMetrics != false) {
-                    $.extend(thisTeamPlayers.club.players[j], thisIdMetrics);
-                }
-                else {
-                    $.extend(thisTeamPlayers.club.players[j], naMetrics);
-                }
-            }
-            if (_monitorModel.attributes.clubModel != null) {
-                _monitorModel.attributes.clubModel.set(thisTeamPlayers);
-                // TODO:  why is trigger required here--why is model udpate not triggering 'change' event?
-                _monitorModel.attributes.clubModel.trigger('change');
-            }
-        }   
-
-        function executeOnePoll(_timeOut) {
-            fakeAMQtimeout = setTimeout(function() {
-                    // amq message handler would really set this:
-                    teamMetrics = teamMetrics;
-                    var timeNow = new Date().getTime()
-                    var updatedValue =  timeNow - lastUpdateTime;
-
-                    writeClubModelPlayers();    
-
-                    updateModel.set({updatedValue: updatedValue});
-                    lastUpdateTime = timeNow;
-                    monitorView.$el.find('.load-report').remove();
-                    executeOnePoll( (1 + Math.random()) * 1000); 
-                }, _timeOut);
-            AMQopen = true;
-        } 
-
-        function rcvMessage(message) {
-            console.log("received "+message);
-            teamMetrics = teamMetrics;
-            var timeNow = new Date().getTime()
-            var updatedValue =  timeNow - lastUpdateTime;
-
-            writeClubModelPlayers();    
-
-            updateModel.set({updatedValue: updatedValue});
-            lastUpdateTime = timeNow;
-            AMQopen = true;
-        }
-
-
-
-        // public methods
-        return {
-
-            start: function() {
-                // start up the amq connection and specify message receipt handler
-                // var amq = org.activemq.Amq;
-                // var chatTopic = "queue://FOO.QUEUE"
-                // amq.init({ 
-                //  uri: 'http://localhost:61616/MyAjaxServlet', 
-                //  logging: true,
-                //  timeout: 2,
-                //  clientId: (new Date()).getTime().toString()
-                // });
-
-                // amq.sendMessage(chatTopic, '<message type="chat" from="scott">message one</message>');
-                // amq.sendMessage(chatTopic, '<message type="chat" from="scott">message two</message>');
-                // amq.sendMessage(chatTopic, '<message type="chat" from="scott">message three</message>');
-                // amq.sendMessage(chatTopic, '<message type="chat" from="scott">message four</message>');
-
-                //amq.addListener('amqc','queue://FOO.QUEUE', rcvMessage);  
-                AMQopen = true;     
-                executeOnePoll(initialTimeOut);
-            },
-            stop: function() {
-                if (AMQopen) {
-                    clearTimeout(fakeAMQtimeout);
-                    AMQopen = false;
-                }
-            }
-        };
-
-
-    };
-
 
 // initialization of whole app on page load
 
 
     return {
         init: function() {
-            console.log("todo app is init'd");
-
-            // setup empty todo collection
-            todoCollection = new TodoCollection();
-
-            // create the app
-            applicationModel = new ApplicationModel();
-            applicationView = new ApplicationView({ el: $("#applicationContainer"), model: applicationModel});
-
+            new ApplicationView({ el: $("#applicationContainer"), collection: new ApplicationCollection() });
         }
 
     }
