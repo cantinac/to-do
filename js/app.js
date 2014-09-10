cc61e35e9d5bc4dd9033b8ba7671d5377577b888
Gerry Salazar <gs@hspindesign.com>
2014-09-09 20:23:25 -0400

    base framework

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.controllers',
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/', {
+      controller: 'MyCtrl1'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider.html5Mode(true);
+});
+
