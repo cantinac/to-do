ec18412e3967b71759ca6d49c95aa92b278013b9
Joe Wilson <joewilson81@gmail.com>
2014-09-21 16:07:51 -0400

    Defined application structure

--- js/app.js
+++ js/app.js
@@ -4,3 +4,57 @@
  * @author Joseph Wilson
  */
 
+var template = function(name) {
+    return Mustache.compile($('#'+name+'-template').html());
+};
+
+var Todo = Backbone.Model.extend({
+    defaults: {
+        description: "",
+        completed: false
+    }
+});
+
+var Todos = Backbone.Collection.extend({
+    model: Todo,
+    localStorage: new Store("todos")
+});
+
+var TodoView = Backbone.View.extend({
+    template: template("todo"),
+    events: {
+        "click .toggle": "toggleTodo"
+    },
+    initialize: function() {
+
+    },
+    render: function() {
+
+    },
+    toggleTodo: function() {
+
+    }
+});
+
+var TodoApp = Backbone.View.extend({
+    el: $("todo-app"),
+    events: {
+        "click button#add-todo": "addTodo"
+    },
+    initialize: function() {
+
+    },
+    render: function() {
+
+    },
+    addTodo: function() {
+
+    },
+    appendTodo: function() {
+
+    }
+});
+
+$(function() {
+    new TodoApp();
+});
