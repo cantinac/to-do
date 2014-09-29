a4198e7c8d42916ca582ca6ce81f356c00d06df8
0restes <lukehollis@gmail.com>
2014-09-29 00:34:16 -0400

    finish styles and bug fixes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
-A demo client-side todo app
+A demo client-side todo app:
+
+1. Clone repository:  git clone https://github.com/0restes/to-do.git
+2. Navigate to directory and Install packages:  cd to-do && npm install
+3. Load file in browser (e.g. navigate to file:///path/to/to-do/index.html) 
+4. Thanks! 
--- app.js
+++ app.js
@@ -20,4 +20,53 @@ app.controller('CantinaController', ['$scope', function($scope) {
 							});
 			$scope.item = '';
 		}
+
+	this.toggleCheck = function(e) {
+			var i
+			,	isChecked = false
+			,	elem = e.target
+			;
+
+			// make sure the target elem is the li
+			if (elem.nodeName !== "LI") {
+				elem = elem.parentElement;
+			}
+
+
+			// check if the li is checked 
+			if(elem.className.indexOf("true") > 0) {
+				isChecked = true;
+			}
+
+			// check or uncheck
+			if ( isChecked ){
+				elem.children[0].checked = false;
+				elem.className = elem.className.replace("true");
+			}else{
+				elem.children[0].checked = true;
+				elem.className = elem.className + " true";
+			}
+		}
+
+	// update the submit button by orientation
+	this.updateSubmitByOrientation = function(orientation){
+		if (orientation === "portrait") {
+			document.form.submit.value = '+';
+			document.body.className = "portrait";
+		}
+	}
+
+
+	// basic function for detecting orientation
+	this.checkOrientation = function() {
+		var orientation = 'landscape';
+		if (window.innerHeight > window.innerWidth) {
+			orientation = 'portrait';
+		}
+
+		this.updateSubmitByOrientation(orientation);
+		}
+	// check orientation on bootstrap
+	this.checkOrientation();
 }]);
+
