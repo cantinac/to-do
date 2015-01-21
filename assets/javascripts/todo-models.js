1d990a44cc8ce70282626becb8624f113c37cc09
David Foresman <foresman@gmail.com>
2015-01-20 22:12:28 -0500

    significant refactoring: updated Todo comparator to properly sort completed and in-progress todos. added todo filter options for All, Complete, and In-Progress. added ability to delete Todos, fixed ViewModel listing issues

--- assets/javascripts/todo-models.js
+++ assets/javascripts/todo-models.js
@@ -11,14 +11,27 @@ var Todo = Backbone.Model.extend({
     this.set('created', new Date());
     this.set(attrs);
 
-    this.on('all', function(ev){
-      console.log('Event!: ', ev)
-    })
+    this.on('destroy', this.onDestroy, this)
   },
 
   toggle: function(){
     return (this.get('completed') === null) ? this.set('completed', new Date()) : this.set('completed', null)
   },
+
+  complete: function(){
+    return this.set('completed', new Date());
+  },
+
+  uncomplete: function(){
+    return this.set('completed', null)
+  },
+
+  onDestroy: function(){
+    this.stopListening()
+    this.off()
+    this.clear()
+  },
+
 })
 
 
@@ -26,5 +39,37 @@ var Todo = Backbone.Model.extend({
 var Todos = Backbone.Collection.extend({
   model: Todo,
 
-  comparator: 'completed',
+  // sort based on created date first, then completed date. separates completed from in-progress
+  comparator: function(a,b){
+
+    var aDate = a.get('completed'),
+        bDate = b.get('completed')
+
+    // if both aren't complete, sort by created date
+    if ((aDate === null) && (bDate === null)) {
+      var aCreate = a.get('created'),
+          bCreate = b.get('created');
+
+      return aCreate > bCreate ? 1 : aCreate < bCreate ? -1 : 0;
+    
+    // if one is complete, it will always come after incomplete
+    } else if (aDate === null) {
+      return -1;
+    } else if (bDate === null) {
+      return 1;
+
+    // if both are complete, sort by completed date  
+    } else {
+      return aDate > bDate ? 1 : aDate < bDate ? -1 : 0;
+    }
+  },
+
+
+  // force the collection to re-sort when an item changes
+  initialize: function(){
+    this.on('change', function(){
+      this.sort();
+    }, this)
+  },
+
 })
--- assets/javascripts/todo-viewmodels.js
+++ assets/javascripts/todo-viewmodels.js
@@ -11,7 +11,26 @@ var cantinaVM = function(){
     todos: new Todos()
   };
 
-  self.todos = kb.collectionObservable(self.collections.todos);
+  self.filterOptions = ko.observableArray(['all', 'complete', 'inProg'])
+  self.filter = ko.observable('all')
+
+  self.todos = kb.collectionObservable(
+    self.collections.todos,
+    todoVM,
+    {
+      filters: function(model){
+        var myFilter = self.filter();
+
+        if (myFilter === 'all') {
+          return true;
+        } else if (myFilter === 'complete') {
+          return model.get('completed') != null;
+        } else {
+          return model.get('completed') === null;
+        }
+      }
+    }
+  );
 
   self.newTodoTitle = ko.observable();
 
@@ -19,7 +38,6 @@ var cantinaVM = function(){
     self.collections.todos.add({title: self.newTodoTitle(), });
     self.newTodoTitle('');
   }
-
 }
 
 
@@ -32,19 +50,16 @@ var todoVM = function(model){
   self.title = kb.observable(model, 'title');
   self.completed = kb.observable(model, 'completed');
 
-  self.isChecked = ko.computed(function(){
-    return this.completed() != null;
-  }, this);
+  self.isChecked = ko.observable(false);
 
-  self.onToggle = function(){
-    self.model.toggle();
-  };
+  self.isChecked.subscribe(function(newvalue){
+    return (newvalue === true) ? model.complete() : model.uncomplete();
+  })
 
+  self.remove = function(){
+    return model.trigger('destroy', model, model.collection, {})
+  }
 }
-
-
-
-
 
 
 
