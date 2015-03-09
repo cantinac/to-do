47afef4e5e224c8f4028b5cdb40e464029593e47
Bob <bob@frieden.org>
2015-03-09 15:08:38 -0400

    Might work

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by robertfrieden on 3/3/15.
+ */
+angular.module('app', ['LocalStorageModule']);
---
+++ js/controllers/mainCtrl.js
@@ -0,0 +1,70 @@
+/**
+ * Created by robertfrieden on 3/8/15.
+ */
+
+angular.module('app')
+    .directive('ngPlaceholder', function() {
+        return {
+            restrict: 'A',
+            scope: {
+                placeholder: '=ngPlaceholder'
+            },
+            link: function(scope, elem, attr) {
+                scope.$watch('placeholder',function() {
+                    elem[0].placeholder = scope.placeholder;
+                });
+            }
+        }
+    })
+    .controller('MainCtrl', function MainCtrl($scope, $filter,localStorageService) {
+        'use strict';
+
+        $scope.placeholder="What do you need to get done?";
+        //Init test data
+        var test=localStorageService.get('todoKey');
+        if (!test){
+            $scope.lstToDo=[
+                {
+                    completed: false,
+                    description: "1st Item"
+                },
+                {
+                    completed: true,
+                    description: "2nd Item"
+                }
+            ]
+            SaveList();
+        }
+        else{
+            $scope.lstToDo=test;
+        }
+
+        $scope.AddNew=function(completed,description){
+            var temp={
+                completed: completed,
+                description: description
+            };
+            if (description) {
+                $scope.lstToDo.push(temp);
+                SaveList();
+                $scope.newTodo="";
+            }
+
+        };
+
+        $scope.CLearAll=function() {
+            $scope.lstToDo=[];
+            SaveList();
+        }
+
+        $scope.toggleCompleted=function(changedToDo)
+        {
+            var i=$scope.lstToDo.indexOf(changedToDo);
+            $scope.lstToDo[i].completed=changedToDo.completed;
+            SaveList();
+        }
+
+        function SaveList(){
+            localStorageService.set('todoKey',$scope.lstToDo);
+        }
+    });
