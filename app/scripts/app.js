20fbf45b6c8b98195eeda727f05be8ec1c5be44f
Michael Etzel <metzel@mocartni.com>
2015-03-17 00:39:14 -0400

    test

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular
+  .module('cantinaApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'LocalStorageModule'
+  ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
